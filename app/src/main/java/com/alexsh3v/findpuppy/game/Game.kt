02993325764a341f8a2a7b47b0b798cca4f0f2bf
AlexSh3v (Ubuntu) <aleksei567gi@gmail.com>
2023-05-04 21:46:08 +0300

    Added moving animation to selected border

--- app/src/main/java/com/alexsh3v/findpuppy/game/Game.kt
+++ app/src/main/java/com/alexsh3v/findpuppy/game/Game.kt
@@ -1,8 +1,9 @@
 package com.alexsh3v.findpuppy.game
 
+import android.annotation.SuppressLint
 import android.util.Log
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
@@ -14,6 +15,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import com.alexsh3v.findpuppy.FindPuppyGame
 import com.alexsh3v.findpuppy.FindPuppyGame.Companion.FIELD_SIZE
 import com.alexsh3v.findpuppy.R
@@ -37,6 +39,7 @@ fun App(game: FindPuppyGame) {
 fun Menu(gameViewModel: FindPuppyGame) {
 }
 
+@SuppressLint("ResourceType")
 @Composable
 fun Game(game: FindPuppyGame) {
 
@@ -54,6 +57,9 @@ fun Game(game: FindPuppyGame) {
     val screenWidth = configuration.screenWidthDp.dp
 
     val scope = rememberCoroutineScope()
+    var isGameSuspendedNecessarily by remember {
+        mutableStateOf(false)
+    }
     var isPuppyFound by remember {
         mutableStateOf(false)
     }
@@ -64,6 +70,32 @@ fun Game(game: FindPuppyGame) {
         selectedTile.i.collectAsState(initial = 0).value,
         selectedTile.j.collectAsState(initial = 0).value
     )
+
+
+    var clickedVector = remember {
+        mutableStateOf(Pair(centerX, centerY))
+    }
+    var isNearestTilePressed by remember {
+        mutableStateOf(false)
+    }
+    val selectedBorderX by animateDpAsState(
+        targetValue = if (isNearestTilePressed && !isPuppyFound) clickedVector.value.first else centerX,
+    )
+    val selectedBorderY by animateDpAsState(
+        targetValue = if (isNearestTilePressed && !isPuppyFound) clickedVector.value.second else centerY
+    )
+    Image(
+        painter = painterResource(id = R.raw.selected_border),
+        contentDescription = "",
+        modifier = Modifier
+            .size(tileSize)
+            .offset(
+                if (isNearestTilePressed) selectedBorderX else centerX,
+                if (isNearestTilePressed) selectedBorderY else centerY
+            )
+            .zIndex(2f)
+    )
+
 
     for (i in 0 until FIELD_SIZE) {
         for (j in 0 until FIELD_SIZE) {
@@ -85,7 +117,7 @@ fun Game(game: FindPuppyGame) {
             )
             val distance = abs(relativeVector.first) + abs(relativeVector.second)
 
-            val relativeCoordinates = Pair(
+            val newCalculatedVector = Pair(
                 tileSize.times(relativeVector.first).plus(centerX), // x
                 tileSize.times(relativeVector.second).plus(centerY) // y
             )
@@ -93,8 +125,8 @@ fun Game(game: FindPuppyGame) {
             var modifier = Modifier
                 .size(tileSize)
                 .offset(
-                    x = relativeCoordinates.first,
-                    y = relativeCoordinates.second
+                    x = newCalculatedVector.first,
+                    y = newCalculatedVector.second
                 )
 
             var colorFilter: ColorFilter? = null
@@ -103,36 +135,49 @@ fun Game(game: FindPuppyGame) {
             // that means that it is the selected tile.
             // So we make yellow border to it
             if (distance == 0) {
-                modifier = modifier.border(3.dp, Color.Yellow)
+//                modifier = modifier.border(3.dp, Color.Yellow)
             }
 
             // For tile that are to the left, right, above and bottom
-            if (!isPuppyFound && distance == 1) {
+            if (!isGameSuspendedNecessarily && distance == 1) {
 
                 colorFilter = ColorFilter.tint(
                     Color(0x43000000), // todo: export color
                     blendMode = BlendMode.Darken
                 )
 
+
                 modifier = modifier.clickable {
-                    if (i == 0 && 0 == j)
-                        tileSize = tileSize.plus(5.dp)
-                    if (i == 0 && 1 == j)
-                        tileSize = tileSize.minus(5.dp)
+                    isNearestTilePressed = true
+                    clickedVector.value = clickedVector.value.copy(
+                        newCalculatedVector.first,
+                        newCalculatedVector.second
+                    )
 
-                    tileObject.changeState(Tile.State.Shown)
-                    Log.d("FindPuppyGame", "clicked at: $i $j")
-                    selectedTile.bindPosition(i, j)
+                    scope.launch {
 
+                        // todo: move into function to automatically set flags
+                        isGameSuspendedNecessarily = true
+                        delay(1000)
+                        isGameSuspendedNecessarily = false
 
-                    if (type == Tile.Type.WithPuppy) {
-                        scope.launch {
+                        tileObject.changeState(Tile.State.Shown)
+                        Log.d("FindPuppyGame", "clicked at: $i $j")
+                        selectedTile.bindPosition(i, j)
+
+                        if (type == Tile.Type.WithPuppy) {
+
+                            isGameSuspendedNecessarily = true
                             isPuppyFound = true
                             delay(2000)
+                            isGameSuspendedNecessarily = false
                             isPuppyFound = false
                             game.generateNewField()
                         }
+
+                        isNearestTilePressed = false
                     }
+
                 }
             }
 
